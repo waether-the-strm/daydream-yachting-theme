94e28845f40464e0df4fad42b12cdaba67d38fe6
Jan Mirecki <mirecki.janek@gmail.com>
2022-02-15 15:01:52 +0200

    Init repo with base wp block theme files

---
+++ functions.php
@@ -0,0 +1,23 @@
+<?php
+
+if ( ! function_exists( 'daydream_support' ) ) :
+	function daydream_support()  {
+
+		// Adding support for core block visual styles.
+		add_theme_support( 'wp-block-styles' );
+
+		// Enqueue editor styles.
+		add_editor_style( 'style.css' );
+	}
+	add_action( 'after_setup_theme', 'daydream_support' );
+endif;
+
+/**
+ * Enqueue scripts and styles.
+ */
+function daydream_scripts() {
+	// Enqueue theme stylesheet.
+	wp_enqueue_style( 'daydream-style', get_template_directory_uri() . '/style.css', array(), wp_get_theme()->get( 'Version' ) );
+}
+
+add_action( 'wp_enqueue_scripts', 'daydream_scripts' );
